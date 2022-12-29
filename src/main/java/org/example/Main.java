4d1c9ef162b59181792d389027bd23fdb57d0dc8
Stanislav Telyakaev <stelyakaev@usetech.ru>
2022-12-29 21:24:32 +0300

    personal data removed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("My name is Stanislav");
+        System.out.println("My name is [Censored]");
     }
 }
